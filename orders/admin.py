f488befee6de3461921a275288e1d23e260ab7b1
Taraslysak <lysak.ipr@gmail.com>
2020-05-15 10:11:08 +0300

    Added pizza models

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Meal, Meal_size, Topping, Pizza_style, Pizza_option, Pizza
+
+admin.site.register(Meal)
+admin.site.register(Meal_size)
+admin.site.register(Topping)
+admin.site.register(Pizza)
+admin.site.register(Pizza_style)
+admin.site.register(Pizza_option)
 # Register your models here.
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+class Meal(models.Model):
+    meal_name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'${self.meal_name}'
+
+
+class Meal_size(models.Model):
+    size = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'${self.size}'
+
+
+class Topping(models.Model):
+    topping = models.CharField(max_length=128)
+
+    def __str__(self):
+       return f'${self.topping}' 
+
+
+class Pizza_style(models.Model):
+    style = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'${self.style}'
+
+
+class Pizza_option(models.Model):
+    option = models.CharField(max_length=128)
+
+    def __str__(self):
+        return f'${self.option}'
+
+
+class Pizza(models.Model):
+    style = models.ForeignKey(Pizza_style, on_delete=models.CASCADE, related_name='pizzas')
+    size = models.ForeignKey(Meal_size, on_delete=models.CASCADE, related_name='pizzas')
+    option = models.ForeignKey(Pizza_option, on_delete=models.CASCADE, related_name='pizzas')
+    price = models.IntegerField()
+
+    def __str__(self):
+        return f'This is ${self.style} pizza of ${self.size} size. Its option is "${self.option}" and price - ${self.price} USD.'
+
