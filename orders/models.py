09d733353ab96c5f8eeb6a72ab2d8f7007ff001c
Taraslysak <lysak.ipr@gmail.com>
2020-05-15 10:29:40 +0300

    Deleted $ signs from __str__ functions in pizza models

--- orders/models.py
+++ orders/models.py
@@ -5,35 +5,35 @@ class Meal(models.Model):
     meal_name = models.CharField(max_length=64)
 
     def __str__(self):
-        return f'${self.meal_name}'
+        return f'{self.meal_name}'
 
 
 class Meal_size(models.Model):
     size = models.CharField(max_length=64)
 
     def __str__(self):
-        return f'${self.size}'
+        return f'{self.size}'
 
 
 class Topping(models.Model):
     topping = models.CharField(max_length=128)
 
     def __str__(self):
-       return f'${self.topping}' 
+       return f'{self.topping}' 
 
 
 class Pizza_style(models.Model):
     style = models.CharField(max_length=64)
 
     def __str__(self):
-        return f'${self.style}'
+        return f'{self.style}'
 
 
 class Pizza_option(models.Model):
     option = models.CharField(max_length=128)
 
     def __str__(self):
-        return f'${self.option}'
+        return f'{self.option}'
 
 
 class Pizza(models.Model):
@@ -43,5 +43,5 @@ class Pizza(models.Model):
     price = models.IntegerField()
 
     def __str__(self):
-        return f'This is ${self.style} pizza of ${self.size} size. Its option is "${self.option}" and price - ${self.price} USD.'
+        return f'This is {self.style} pizza of {self.size} size. Its option is "{self.option}" and price - {self.price} USD.'
 
